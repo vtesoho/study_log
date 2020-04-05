1bff6adc206139e6665b2a60ebb1eb1e2e4f8200
vtesoho <vtesoho@163.com>
2020-04-05 22:33:06 +0800

    js

---
+++ js/test.js
@@ -0,0 +1,12 @@
+var arr = new Array(6)
+arr[0] = "George"
+arr[1] = "John"
+arr[2] = "Thomas"
+arr[3] = "James"
+arr[4] = "Adrew"
+arr[5] = "Martin"
+
+
+console.log(arr.slice(0,2))
+console.log(arr.slice(2,4))
+console.log(arr.slice(4,10))
