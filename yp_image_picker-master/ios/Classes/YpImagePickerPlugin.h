31e86779413a60a7e24d816995421c6d31163b13
vteso <vtesoho@163.com>
2020-08-03 20:50:48 +0800

    子查询

--- yp_image_picker-master/ios/Classes/YpImagePickerPlugin.h
+++
@@ -1,4 +0,0 @@
-#import <Flutter/Flutter.h>
-
-@interface YpImagePickerPlugin : NSObject<FlutterPlugin>
-@end
--- yp_image_picker-master/ios/Classes/YpImagePickerPlugin.m
+++
@@ -1,8 +0,0 @@
-#import "YpImagePickerPlugin.h"
-#import <yp_image_picker/yp_image_picker-Swift.h>
-
-@implementation YpImagePickerPlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [SwiftYpImagePickerPlugin registerWithRegistrar:registrar];
-}
-@end
