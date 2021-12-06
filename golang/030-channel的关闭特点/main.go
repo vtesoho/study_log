85c10c442e294b681b40d8ea7b7894bd653a9573
305976564@qq.com <305976564@qq.com>
2021-12-06 20:49:10 +0800

    030-channel的关闭特点

---
+++ golang/030-channel的关闭特点/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	c := make(chan int)
+
+	go func() {
+		for i := 0; i < 5; i++ {
+			c <- i
+		}
+
+		//close可以关闭一个channel
+		close(c)
+	}()
+
+	for {
+		//ok如果为true表示channel没有关闭，如果为false表示channcel已经关闭
+		if data, ok := <-c; ok {
+			fmt.Println("data", data)
+		} else {
+			break
+		}
+	}
+
+	fmt.Println("main 结束")
+
+}
