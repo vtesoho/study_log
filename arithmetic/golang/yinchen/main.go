a6065c8a7299733695421131a3c7e7c15284001f
vteso <vtesoho@163.com>
2022-03-08 20:12:25 +0800

    quicksort

--- arithmetic/golang/yinchen/main.go
+++ arithmetic/golang/yinchen/main.go
@@ -9,5 +9,5 @@ func main() {
 	// main1()
 	arr := []int{4, 8, 1, 9, 3, 7, 2, 5, 10}
 	// fmt.Println(shellsort.ShellSort(arr))
-	fmt.Println(quicksort.Quicksort(arr))
+	fmt.Println(quicksort.Quicksort2(arr))
 }
--- arithmetic/golang/yinchen/src/quicksort/quicksort.go
+++ arithmetic/golang/yinchen/src/quicksort/quicksort.go
@@ -1,5 +1,7 @@
 package quicksort
 
+import "math/rand"
+
 func Quicksort(arr []int) []int {
 	length := len(arr)
 	if length <= 1 {
@@ -12,6 +14,37 @@ func Quicksort(arr []int) []int {
 		mid := make([]int, 0)
 		mid = append(mid, splitdata)
 		for i := 1; i < length; i++ {
+			if arr[i] < splitdata {
+				low = append(low, arr[i])
+			} else if arr[i] > splitdata {
+				high = append(high, arr[i])
+			} else {
+				mid = append(mid, arr[i])
+			}
+		}
+		low, high = Quicksort(low), Quicksort(high)
+		myarr := append(append(low, mid...), high...)
+		return myarr
+
+	}
+}
+
+//取一个随机树
+func Quicksort2(arr []int) []int {
+	length := len(arr)
+	if length <= 1 {
+		return arr
+	} else {
+		n := rand.Int() % length
+		splitdata := arr[n]
+		low := make([]int, 0)
+		high := make([]int, 0)
+		mid := make([]int, 0)
+		mid = append(mid, splitdata)
+		for i := 0; i < length; i++ {
+			if i == n {
+				continue
+			}
 			if arr[i] < splitdata {
 				low = append(low, arr[i])
 			} else if arr[i] > splitdata {
