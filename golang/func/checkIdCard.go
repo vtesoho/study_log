86a0a4603e29cba3cdd153afd5bb629090b08f9b
vteso <vtesoho@163.com>
2022-03-26 09:42:28 +0800

    checkidcard

---
+++ golang/func/checkIdCard.go
@@ -0,0 +1,15 @@
+// CheckIdCard 检验身份证
+func CheckIdCard(card string) bool {
+	//18位身份证 ^(\d{17})([0-9]|X)$
+	// 匹配规则
+	// (^\d{15}$) 15位身份证
+	// (^\d{18}$) 18位身份证
+	// (^\d{17}(\d|X|x)$) 18位身份证 最后一位为X的用户
+	regRuler := "(^\\d{15}$)|(^\\d{18}$)|(^\\d{17}(\\d|X|x)$)"
+
+	// 正则调用规则
+	reg := regexp.MustCompile(regRuler)
+
+	// 返回 MatchString 是否匹配
+	return reg.MatchString(card)
+}
