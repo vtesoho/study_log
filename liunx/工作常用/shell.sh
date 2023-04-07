2583671038684b4f75be698aba125fea38e0337d
vteso <vtesoho@163.com>
2023-04-07 23:47:36 +0800

    shell

--- liunx/工作常用/shell.sh
+++ liunx/工作常用/shell.sh
@@ -13,22 +13,37 @@ do
 
 done
 
-ssh root@172.28.162.55 "supervisorctl stop all"
-ssh root@172.28.162.83 "supervisorctl stop all"
-ssh root@172.28.162.85 "supervisorctl stop all"
+
 
 for i in {42815..42819}
 do
 
+    ssh root@172.28.162.55 "supervisorctl stop black${i}"
     scp /root/blackapitwo/main${i} root@172.28.162.55:/root/blackapitwo/main${i}
+    ssh root@172.28.162.55 "supervisorctl start black${i}"
+
+    ssh root@172.28.162.83 "supervisorctl stop black${i}"
     scp /root/blackapitwo/main${i} root@172.28.162.83:/root/blackapitwo/main${i}
+    ssh root@172.28.162.83 "supervisorctl start black${i}"
+
+    ssh root@172.28.162.85 "supervisorctl stop black${i}"
     scp /root/blackapitwo/main${i} root@172.28.162.85:/root/blackapitwo/main${i}
+    ssh root@172.28.162.85 "supervisorctl start black${i}"
 
 done
 
-ssh root@172.28.162.55 "supervisorctl start all"
-ssh root@172.28.162.83 "supervisorctl start all"
-ssh root@172.28.162.85 "supervisorctl start all"
+# for i in {42816..42819}
+# do
+
+#     ssh root@172.28.162.55 "supervisorctl start black${i}"
+#     ssh root@172.28.162.83 "supervisorctl start black${i}"
+#     ssh root@172.28.162.85 "supervisorctl start black${i}"
+
+# done
+
+# ssh root@172.28.162.55 "supervisorctl start all"
+# ssh root@172.28.162.83 "supervisorctl start all"
+# ssh root@172.28.162.85 "supervisorctl start all"
 
 
 # for i in {42815..42819}
