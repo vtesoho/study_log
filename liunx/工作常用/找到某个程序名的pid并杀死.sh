f70153997ec403b3d217f66cd0912b12881e9928
vteso <vtesoho@163.com>
2023-04-11 17:49:31 +0800

    0411

---
+++ liunx/工作常用/找到某个程序名的pid并杀死.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+go build 
+
+# 获取程序名
+program_name="maintest"
+
+# 查找PID
+pid=$(ps aux | grep $program_name | awk '{print $2}')
+
+# 判断PID是否存在，如果不存在则退出
+if [ -z $pid ]
+then
+    echo "Process not found."
+    exit 1
+fi
+
+# 杀死进程
+kill ps aux | grep $program_name | awk '{print $2}
+
+# 输出操作结果
+if [ $? -eq 0 ]
+then
+    echo "Process successfully killed."
+else
+    echo "Error killing process."
+fi
